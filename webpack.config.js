4e6b6f14e468ef8fb24f4c3f2648c9838b670a61
johnedisc <christopher.johnedis@gmail.com>
2023-02-03 00:57:53 -0800

    add eslint, rc file, and add it into scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -17,6 +18,7 @@ module.exports = {
     },
   },
   plugins: [
+    new ESLintPlugin(),
     new CleanWebpackPlugin({
       verbose: true
     }),
